bc4023551774cb56cb9c858325ddd156c037b1f3
Gustavo Vaz <104520068+Gustavonuva@users.noreply.github.com>
2023-02-21 10:41:22 -0300

    adicionando github action

--- src/components/GaInputText/index.tsx
+++ src/components/GaInputText/index.tsx
@@ -44,7 +44,6 @@ export const GaInputText = ({
         type={type}
         value={value}
         onChange={event => onChange(event.target.value)}
-        placeholder="seuemail@gmail.com.br"
       />
     </div>
   );
